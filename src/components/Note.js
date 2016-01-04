fd4d1b9b61f0c535703a0488cc65789b3fbf2c4b
Anita Amini <baadge29@yahoo.co.uk>
2016-01-04 17:34:31 +0000

    note basic functionality working

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,24 +3,29 @@ import React from 'react';
 export default class Note extends React.Component {
     constructor(props) {
        super(props); 
-       this.state = { likes: 0}
+       this.state = { likes: 0, note: ''}
     }
     
-    handleLikes(e) {
-    const count = this.state.likes + 1;
+    handleLikes() {
+        const count = this.state.likes + 1;
         this.setState({ likes: count })
     }
     
-    handleUnlikes(e) {
-    const count = this.state.likes - 1;
+    handleUnlikes() {
+        const count = this.state.likes - 1;
         this.setState({ likes: count })
+    }
+    
+    saveNote(e) {
+        const text = e.target.value;
+        this.setState({ note: text });
     }
     
     render() {
         return (
             <div>
-                <textarea></textarea>
-                <br />
+                <textarea ref="text" onChange={this.saveNote.bind(this)}></textarea>
+                <p>{this.state.note}</p>
                 <button onClick={this.handleLikes.bind(this)}>Like</button>
                 <button onClick={this.handleUnlikes.bind(this)}>Unlike</button>
                 <p>{this.state.likes}</p>
