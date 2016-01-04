85b55f1084932fec9ccf620134324cbc69339296
Anita Amini <baadge29@yahoo.co.uk>
2016-01-04 15:37:45 +0000

    state, the react way

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,15 +3,17 @@ import React from 'react';
 export default class Note extends React.Component {
     constructor(props) {
        super(props); 
-       this.state = { likes: 0, unlikes: 0}
+       this.state = { likes: 0}
     }
     
     handleLikes(e) {
-        this.setState({ likes: +1 })
+    const count = this.state.likes + 1;
+        this.setState({ likes: count })
     }
     
     handleUnlikes(e) {
-        this.setState({ unlikes: -1 })
+    const count = this.state.likes - 1;
+        this.setState({ likes: count })
     }
     
     render() {
@@ -22,7 +24,6 @@ export default class Note extends React.Component {
                 <button onClick={this.handleLikes.bind(this)}>Like</button>
                 <button onClick={this.handleUnlikes.bind(this)}>Unlike</button>
                 <p>{this.state.likes}</p>
-                <p>{this.state.unlikes}</p>
                 <button>Delete</button>
             </div>
         )
